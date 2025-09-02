0be797277408f545cd37d7cb18f0af0622e16033
Sara Lopez Gutierrez <slopezgu@emeal.nttdata.com>
2025-09-02 23:56:08 +0200

    Added user storage and gitignore

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,6 +1,7 @@
 from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 @dag
 def user_processing():
@@ -51,6 +52,7 @@ def user_processing():
     @task
     def process_user(user_info):
         import csv
+        from datetime import datetime
 
         user_info = {
             "id": "123",
@@ -58,14 +60,25 @@ def user_processing():
             "lastname": "Doe",
             "email": "john.doe@example.com",
         }
+        user_info["created_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         with open("/tmp/user_info.csv", "w", newline="") as f:
             writter = csv.DictWriter(f, fieldnames=user_info.keys())
             writter.writeheader()
             writter.writerow(user_info)
+
+    @task
+    def store_user():
+        hook = PostgresHook(postgres_conn_id="postgres")
+        hook.copy_expert(
+            sql="COPY users FROM STDIN WITH CSV HEADER",
+            filename="/tmp/user_info.csv"
+        )
     
     fake_user=is_api_available()
     user_info=extract_user(fake_user)
     process_user(user_info)
+    store_user()
+
 
 user_processing()
